112d75762de938938348dc2a2b0a6e4b4fc24c17
Lai Tuan Anh <laituananh1711@gmail.com>
2023-11-23 00:38:29 +0900

    initial commit

---
+++ single-ec2/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "single_instance" {
+    ami = "ami-0230bd60aa48260c6"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "single-instance"
+    }
+}
