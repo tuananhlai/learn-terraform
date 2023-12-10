87ae0bef6671da54973864b7ffab6fce5baf1025
Lai Tuan Anh <laituananh1711@gmail.com>
2023-12-10 17:50:12 +0900

    feat/elastic-wordpress: use remote state

--- elastic-wordpress/terraform.tf
+++ elastic-wordpress/terraform.tf
@@ -1,9 +1,19 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws",
-        version = "5.26.0"
+      source  = "hashicorp/aws",
+      version = "5.26.0"
     }
+  }
+
+  backend "s3" {
+    # Key needs to be different for EVERY terraform project.
+    key = "global/elastic-wordpress/terraform.tfstate"
+
+    bucket         = "terraform-remote-state-198036150276"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
 
