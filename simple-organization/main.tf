0b7da380824e9aa961e7c11f220714abd9230d6d
Lai Tuan Anh <laituananh1711@gmail.com>
2024-03-13 16:15:56 +0900

    feat/simple-organization: initialize

---
+++ simple-organization/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_organizations_organization" "default" {
+  enabled_policy_types = ["SERVICE_CONTROL_POLICY"]
+}
+
+resource "aws_organizations_organizational_unit" "sales" {
+  name      = "Sales"
+  parent_id = aws_organizations_organization.default.roots.0.id
+}
+
+data "aws_iam_policy_document" "s3_readonly" {
+  statement {
+    effect      = "Deny"
+    not_actions = ["s3:Get*", "s3:List*"]
+    resources   = ["arn:aws:s3:::*"]
+  }
+}
+
+resource "aws_organizations_policy" "s3_readonly" {
+  name    = "S3ReadOnly"
+  content = data.aws_iam_policy_document.s3_readonly.json
+}
+
+resource "aws_organizations_policy_attachment" "sales_s3readonly" {
+  target_id = aws_organizations_organizational_unit.sales.id
+  policy_id = aws_organizations_policy.s3_readonly.id
+}
