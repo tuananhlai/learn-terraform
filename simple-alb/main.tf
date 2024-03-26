b46d9c726ca9201bf6d19d710c8b77afc9b84f41
Lai Tuan Anh <laituananh1711@gmail.com>
2024-03-26 21:25:08 +0900

    refactor/simple-alb: use modules

--- simple-alb/main.tf
+++ simple-alb/main.tf
@@ -2,40 +2,56 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  cidr = "10.0.0.0/16"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name                    = "SimpleAlbVpc"
-  cidr                    = "10.0.0.0/16"
-  azs                     = ["us-east-1a", "us-east-1b"]
-  public_subnets          = ["10.0.16.0/20", "10.0.32.0/20"]
+  cidr                    = local.cidr
+  azs                     = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets          = cidrsubnets(local.cidr, 4, 4)
   map_public_ip_on_launch = true
 }
 
-resource "aws_security_group" "instance_sg" {
-  vpc_id = module.vpc.vpc_id
+module "instance_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  vpc_id          = module.vpc.vpc_id
+  name            = "instance-sg-"
+  use_name_prefix = true
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::0/0"]
-  }
+  egress_with_cidr_blocks = [
+    {
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      cidr_blocks      = "0.0.0.0/0"
+      ipv6_cidr_blocks = "::0/0"
+    }
+  ]
 }
 
 resource "aws_instance" "instances" {
@@ -53,7 +69,7 @@ resource "aws_instance" "instances" {
   }
   ami                         = "ami-0230bd60aa48260c6" #Amazon Linux 2023
   instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.instance_sg.id]
+  vpc_security_group_ids      = [module.instance_sg.security_group_id]
   subnet_id                   = each.value.subnet_id
   user_data                   = <<EOF
 #!/bin/bash
@@ -72,23 +88,38 @@ EOF
   user_data_replace_on_change = true
 }
 
-resource "aws_security_group" "lb_sg" {
-  vpc_id = module.vpc.vpc_id
+module "lb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  vpc_id          = module.vpc.vpc_id
+  name            = "lb-sg-"
+  use_name_prefix = true
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::0/0"]
-  }
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      cidr_blocks      = "0.0.0.0/0"
+      ipv6_cidr_blocks = "::0/0"
+    }
+  ]
 }
 
 resource "aws_alb_target_group" "default" {
@@ -106,14 +137,14 @@ resource "aws_alb_target_group_attachment" "attachments" {
 
 resource "aws_alb" "default" {
   subnets         = module.vpc.public_subnets
-  security_groups = [aws_security_group.lb_sg.id]
+  security_groups = [module.lb_sg.security_group_id]
   internal        = false
 }
 
 resource "aws_alb_listener" "default" {
   load_balancer_arn = aws_alb.default.arn
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
   default_action {
     type             = "forward"
     target_group_arn = aws_alb_target_group.default.arn
