8584b245d1650e04104674115a20efde90c9fc6f
Lai Tuan Anh <42168740+tuananhlai@users.noreply.github.com>
2025-05-03 09:22:15 +0900

    feat/simple-ecs: initialize

---
+++ simple-ecs/main.tf
@@ -0,0 +1,106 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  default_vpc_cidr = "10.0.0.0/16"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name                 = "simple-ecs-vpc"
+  cidr                 = local.default_vpc_cidr
+  azs                  = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets       = cidrsubnets(local.default_vpc_cidr, 4, 4)
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+module "instance_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  vpc_id          = module.vpc.vpc_id
+  name            = "simple-ecs-instance-sg"
+  use_name_prefix = true
+
+  // For demo purpose only. The ingress and egress rules should be much more restrictive in production.
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      cidr_blocks      = "0.0.0.0/0"
+      ipv6_cidr_blocks = "::0/0"
+    }
+  ]
+}
+
+data "aws_ami" "amz_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-20*-kernel-6.1-x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+resource "aws_launch_template" "ecs_lt" {
+  name_prefix            = "simple-ecs-launch-template"
+  image_id               = data.aws_ami.amz_linux_2023.id
+  instance_type          = "t3.micro"
+  key_name               = "ec2ecsglog"
+  vpc_security_group_ids = [module.instance_sg.security_group_id]
+
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 30
+      volume_type = "gp2"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
+
+  user_data = base64encode(<<EOF
+#!/bin/bash
+echo ECS_CLUSTER=my-ecs-cluster >> /etc/ecs/ecs.config
+    EOF
+  )
+}
